f5fdb10baba206436ccaa556c45d92bbfeee6371
Gilles Daviet <gdaviet@gmail.com>
2019-12-27 21:06:03 +0100

    Enforce current time in correct timezone

---
+++ collectives/helpers.py
@@ -0,0 +1,13 @@
+from flask import current_app
+from datetime import datetime
+from dateutil import tz
+
+def current_time():
+    tz_name = current_app.config['TZ_NAME']
+    tz_info = tz.gettz(tz_name)
+    now = datetime.now(tz_info)
+    # Datetimes are stored in naive format, assumed to
+    # always be in the correct timezone
+    # For Python to allow comparisons we need to strip the 
+    # tz information from our local time
+    return now.replace(tzinfo = None)
