6c5bf37f749648a184cddbe4cc536a33306a5eef
jnguiot <45011516+jnguiot@users.noreply.github.com>
2020-01-04 19:19:56 +0100

    Cleaning: remove now useless solidpanel css class, remove now useless event macro, remove a useless configuration parameter, add commentaries

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+# All configuration listed here is defaut
+# parameters. You can override them in
+# instance/config.py
+
 import os
 from os import environ
+
+
 
 FLASK_ENV = environ.get('FLASK_ENV')
 FLASK_DEBUG = environ.get('FLASK_DEBUG')
@@ -7,6 +13,7 @@ FLASK_DEBUG = environ.get('FLASK_DEBUG')
 # To generate a new secret key:
 # >>> import random, string
 # >>> "".join([random.choice(string.printable) for _ in range(24)])
+# Secret key can also be setted in instance/config.py
 SECRET_KEY = environ.get('SECRET_KEY') or "'@GU^CpusZ0G2\"`=^QAt\rF]|('"
 
 # Password for admin account
@@ -14,7 +21,7 @@ ADMINPWD = environ.get('ADMINPWD') or "foobar2"
 
 # User/password for accessing extranet API
 default_wsdl = 'https://extranet-clubalpin.com/app/soap/extranet_pro.wsdl'
-EXTRANET_DISABLE = environ.get('EXTRANET_DISABLE') 
+EXTRANET_DISABLE = environ.get('EXTRANET_DISABLE')
 EXTRANET_WSDL = environ.get('EXTRANET_WSDL') or default_wsdl
 EXTRANET_ACCOUNT_ID = environ.get('EXTRANET_ACCOUNT_ID')
 EXTRANET_ACCOUNT_PWD = environ.get('EXTRANET_ACCOUNT_PWD')
@@ -25,9 +32,8 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Page information
-MOTTO = "L'esprit Club Alpin"
-TITLE = "Collectives CAF Annecy" 
-LOGO = "caf/caf-gris.png" 
+TITLE = "Collectives CAF Annecy"
+LOGO = "caf/caf-gris.png"
 #FAVICON= "img/icon/favicon.ico"
 FAVICON = "caf/favicon.ico"
 
@@ -51,7 +57,7 @@ TYPES = {
 UPLOADED_PHOTOS_DEST = os.path.join(basedir,
                                     "collectives/static/uploads")
 UPLOADED_AVATARS_DEST = os.path.join(basedir,
-                                     "collectives/static/uploads/avatars")
+                                    "collectives/static/uploads/avatars")
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
