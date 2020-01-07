77f1c70ef4e37e8b01965dfff4b075c842e971ec
jnguiot <45011516+jnguiot@users.noreply.github.com>
2020-01-07 23:40:44 +0100

    CSV: fix #55 by retrying file import if an Encoding Error is encountered

--- collectives/utils/csv.py
+++ collectives/utils/csv.py
@@ -1,6 +1,7 @@
-from ..models import User
+from ..models import User, Event, db
 from datetime import datetime
-import json
+import json, csv, codecs
+from flask import flash
 
 def fill_from_csv(event, row):
             event.title = row['title']
@@ -21,3 +22,39 @@ def fill_from_csv(event, row):
 
 def convert_csv_time(date_time_str):
     return datetime.strptime(date_time_str, '%d/%m/%y %H:%M')
+
+def process_stream(base_stream, activity_type):
+    # First, we try to decode csv file as utf8
+    # If it fails, we try again as Windows encoding
+    try :
+        stream = codecs.iterdecode(base_stream, "utf8")
+        events, processed, failed = csv_to_events(stream)
+    except UnicodeDecodeError:
+        base_stream.seek(0)
+        stream = codecs.iterdecode(base_stream, "iso-8859-1")
+        events, processed, failed = csv_to_events(stream)
+
+    # Complete event before adding it to db
+    for event in events:
+        event.activity_types = [activity_type]
+        db.session.add(event)
+
+    db.session.commit()
+    return processed, failed
+
+def csv_to_events(stream):
+    events=[]
+    processed = 0
+    failed = 0
+
+    reader = csv.DictReader( stream, delimiter=",")
+    for row in reader:
+        processed += 1
+        event = Event()
+        try:
+            fill_from_csv(event, row)
+            events.append(event)
+        except Exception as e:
+            failed += 1
+            flash(f'Impossible d\'importer la ligne {processed+1}: [{type(e).__name__}] {str(e)} {str(row)}', 'error')
+    return events, processed, failed
