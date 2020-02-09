af0aa0c47e7f1b2a8155a648c6c0bc76a91301cf
jnguiot <45011516+jnguiot@users.noreply.github.com>
2020-02-09 22:50:32 +0100

    Add comment about the __len__ ChoiceEnum

--- collectives/models/utils.py
+++ collectives/models/utils.py
@@ -15,5 +15,6 @@ class ChoiceEnum(enum.IntEnum):
             cls = self.__class__
             return cls.display_names()[self.value]
 
+        # Add a bogus len to avoid crash if a WTForm Length Validator try to get ChoiceEnum length
         def __len__(self):
             return 1
