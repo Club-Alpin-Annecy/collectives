aaf4a48f117cb26281d1fa80e7ace798c81fdf49
jnguiot <45011516+jnguiot@users.noreply.github.com>
2020-02-29 15:03:30 +0100

    Legal: add a legal page

--- collectives/routes/root.py
+++ collectives/routes/root.py
@@ -1,4 +1,5 @@
 from flask import redirect, url_for, Blueprint
+from flask import current_app, render_template
 
 blueprint = Blueprint('root', __name__)
 
@@ -7,3 +8,8 @@ blueprint = Blueprint('root', __name__)
 @blueprint.route('/list')
 def index():
     return redirect(url_for('event.index'))
+
+@blueprint.route('/legal')
+def legal():
+    return render_template('legal.html',
+                           conf=current_app.config)
