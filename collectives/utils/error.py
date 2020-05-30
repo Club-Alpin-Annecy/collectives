a19bb73f47f9599f04b3227ed87640f675f884d5
jnguiot <45011516+jnguiot@users.noreply.github.com>
2020-05-30 10:50:08 +0200

    [FIX] Fix #268: no 302 for not found files

--- collectives/utils/error.py
+++ collectives/utils/error.py
@@ -5,11 +5,19 @@ from flask import redirect, url_for, flash, request, render_template
 
 def not_found(e):
     # pylint: disable=unused-argument
-    """ If URL is unknown, redirect client and display an error message.
+    """ If URL is unknown, redirect client or/and display an error message.
 
     :param e: exception which generated the error page
     :return: a redirection to index page
     """
+    if request.path.split("/")[1] == "static":
+        return "404 Error : Not Found", 404
+
+    # If there is a dot in the last part of the URL, it is a file, and there is
+    # no redirection
+    if "." in request.path.split("/")[-1]:
+        return "404 Error : Not Found", 404
+
     flash(f"Page inconnue: {request.path}")
     return redirect(url_for("event.index"))
 
