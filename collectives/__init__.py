92fd3d910ea26b142d0be38f164d5b11337929d7
François Béruard <f.beruard@overkiz.com>
2020-06-01 15:51:00 +0200

    Remove id, eg activate scrollbar for all editor)

--- collectives/__init__.py
+++ collectives/__init__.py
@@ -59,11 +59,7 @@ def create_app(config_filename="config"):
         filters = "pyscss"
         if app.config["ENV"] == "production":
             filters = "pyscss, cssmin"
-        scss = Bundle(
-            "css/all.scss",
-            filters=filters,
-            output="dist/css/all.css",
-        )
+        scss = Bundle("css/all.scss", filters=filters, output="dist/css/all.css")
         assets.register("scss_all", scss)
 
         # Register blueprints
