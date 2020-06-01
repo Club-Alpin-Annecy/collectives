1f4d45eccc763578beaaaadd135697e375f5f52a
Greg Hoin <1416421+gregone@users.noreply.github.com>
2020-06-01 13:43:15 +0200

    [INTERNAL] Rename files to scss, add cssmin and minify in prod (#281)
    * rename files to scss, add cssmin and minify in prod
    
    * Re-add stand-alone print-event css file

--- collectives/__init__.py
+++ collectives/__init__.py
@@ -55,14 +55,13 @@ def create_app(config_filename="config"):
     with app.app_context():
         # Initialize asset compilation
         assets = Environment(app)
+
+        filters = "pyscss"
+        if app.config["ENV"] == "production":
+            filters = "pyscss, cssmin"
         scss = Bundle(
-            "css/main.css",
-            "css/profile.css",
-            "css/administration.css",
-            "caf/icon/activity.css",
-            "css/event/edit.css",
-            "css/event/event.css",
-            filters="pyscss",
+            "css/all.scss",
+            filters=filters,
             output="dist/css/all.css",
         )
         assets.register("scss_all", scss)
