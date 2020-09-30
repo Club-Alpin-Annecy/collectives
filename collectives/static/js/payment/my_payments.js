34e91826f690621f0cfdd8d73a7b420e7590b94c
Gilles Daviet <gdaviet@gmail.com>
2020-09-30 21:35:44 +0200

    Adress #358 comments

--- collectives/static/js/payment/my_payments.js
+++ collectives/static/js/payment/my_payments.js
@@ -1,20 +1,25 @@
 
-var cellClicked = false;
 
 function actionFormatter(cell, formatterParams, onRendered) {
     if (cell.getValue()) {
-        return '<form style="display:inline; padding:0" action="' + cell.getValue() + '" method="' + formatterParams['method'] + '" ></form><input type="image" src="/static/img/icon/ionicon/md-' + formatterParams['icon'] + '.svg" style="margin: 0;height: 1.2em; width: 1.2em"  alt="' + formatterParams['alt'] + '" title="' + formatterParams['alt'] + '"/>';
+        return `<form style="display:inline; padding:0" action="${cell.getValue()}" method="${formatterParams['method']}" ></form><input type="image" src="/static/img/icon/ionicon/md-${formatterParams['icon']}.svg" style="margin: 0;height: 1.2em; width: 1.2em"  alt="${formatterParams['alt']}" title="${formatterParams['alt']}"/>`;
     }
     return '';
 }
 
-function onclickTriggerInsideForm(e, cell) {
-    if (!cell.getValue()) return;
-    cellClicked = true;
-    cell._cell.element.querySelector('form').submit();
+
+function makeCellClickedCallback(state) {
+    return function (e, cell) {
+        // If the cell does not have a valid receipt link, ignore
+        if (!cell.getValue()) return;
+        // Disarm 'rowClick' event
+        state.cellClicked = true;
+        // Submit form
+        cell._cell.element.querySelector('form').submit();
+    };
 }
 
-function tableColumns(finalized) {
+function tableColumns(finalized, state) {
     var columns = [
         { title: "Date", field: (finalized ? "finalization_time" : "creation_time"), widthGrow: 1, headerFilter: true },
         { title: "Événement", field: "event_title", widthGrow: 2, headerFilter: true },
@@ -26,21 +31,28 @@ function tableColumns(finalized) {
 
     if (finalized) {
         columns.push(
-            { title: "Justificatif", field: "receipt_uri", formatter: actionFormatter, formatterParams: { 'icon': 'ribbon', 'method': 'GET', 'alt': 'Justificatif' }, cellClick: onclickTriggerInsideForm, headerSort: false }
+            { title: "Justificatif", field: "receipt_uri", formatter: actionFormatter, formatterParams: { 'icon': 'document', 'method': 'GET', 'alt': 'Justificatif' }, cellClick: makeCellClickedCallback(state), headerSort: false }
         );
     }
     return columns;
 }
 
 function createMyPaymentsTable(id, url, finalized) {
+
+    // Hack to prevent Tabulator rowClick() event from firing
+    // once a cellClick() has been processed
+    var state = {
+        cellClicked: false
+    };
+
     new Tabulator(id,
         {
             ajaxURL: url,
             layout: "fitColumns",
-            columns: tableColumns(finalized),
+            columns: tableColumns(finalized, state),
 
             rowClick: function (e, row) {
-                if (cellClicked) return;
+                if (state.cellClicked) return;
                 document.location.href = row.getData().details_uri;
             },
 
