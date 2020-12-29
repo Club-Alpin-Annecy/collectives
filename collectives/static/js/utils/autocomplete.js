502f5a3d869ac1c3777281a077c488018df0ffa5
Gilles Daviet <gdaviet@gmail.com>
2020-12-29 17:30:50 +0100

    Slight refactor of autocomplete default arguments

--- collectives/static/js/utils/autocomplete.js
+++ collectives/static/js/utils/autocomplete.js
@@ -1,32 +1,40 @@
 
+/**
+ * Default values for setupAutoComplete() optional settings
+ */
+function autoCompleteDefaultSettings() {
+    return {
+        minChars: 2,  // Minimum number of characters to start loading results
+        maxResults: 8, // Maximum number of results to load
+        itemClass: "autocomplete-suggestion", // CSS class of loaded results containers 
+        // Function rendering the HTML for a given result
+        itemInnerHTML: function (item, itemValue) { return `<span>${itemValue}</span>`; }
+    }
+}
 
 /**
- * Setups automcomplete for a text field
+ * Setups autocomplete for a text field
  * @param {string} field The HTML element
- * @param {string} baseUrl The API url providing the results. Search term will be passed to the 'q' parameter
+ * @param {string} baseUrl The API url providing the results. Search term will be passed to the 'q' parameter, maxResults to the 'l' parameter
  * @param {function} itemValue Function extracting the value associated to an item from the API call result
  * @param {function} onSelect Function called when user select a suggestion result. Function will be passed item id and value.
- * @param {dict} settings  Optional settings
+ * @param {dict} settings  Optional settings -- see autoCompleteDefaultSettings
  */
 function setupAutoComplete(
-    field, 
+    field,
     baseUrl,
     itemValue,
     onSelect,
-    settings =
-    {
-        minChars : 2,
-        itemClass : "autocomplete-suggestion",
-        itemInnerHTML : function(item) { return `<span>${itemValue(item)}</span>`; } 
-    }
-)
-{
-    if(!field) { return null ; }
+    settings = {}
+) {
+    if (!field) { return null; }
+    settings = Object.assign(autoCompleteDefaultSettings(itemValue), settings);
 
     // Functor performing the API query with 'term' then calling 'then'
-    const loadResults  = function(term, then) {
+    const loadResults = function (term, then) {
         var url = new URL(baseUrl, document.baseURI);
         url.searchParams.set("q", term);
+        url.searchParams.set("l", settings.maxResults);
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url);
         xhr.setRequestHeader('Content-Type', 'application/json');
@@ -39,15 +47,13 @@ function setupAutoComplete(
     };
 
     //  Functor calling 'onSelect' with id and value from the select item
-    const onSelectInternal = function(e, term, item)
-    {
+    const onSelectInternal = function (e, term, item) {
         onSelect(item.getAttribute('data-id'), item.getAttribute('data-val'));
     }
 
-    const renderItem =  function(item)
-    {
+    const renderItem = function (item) {
         var val = itemValue(item);
-        var innerHTML = settings.itemInnerHTML(item);
+        var innerHTML = settings.itemInnerHTML(item, val);
         return `<div class="${settings.itemClass}" data-val="${val}" data-id="${item.id}">${innerHTML}</div>`
     };
 
