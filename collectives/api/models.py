602945ddc7fdff69d28fae0cb4bb19e3e6e53e76
jnguiot <45011516+jnguiot@users.noreply.github.com>
2021-05-15 18:42:01 +0200

    [FEATURE] Add filter to profile event list

--- collectives/api/models.py
+++ collectives/api/models.py
@@ -7,7 +7,7 @@ from flask import Response
 
 from .common import blueprint
 from ..models.utils import ChoiceEnum
-from ..models import ActivityType
+from ..models import ActivityType, EventTag
 
 
 @blueprint.route("/models.js")
@@ -19,4 +19,8 @@ def models_to_js():
             enums = enums + "const Enum" + name + "=" + obj.js_values() + ";"
 
     enums = enums + "const EnumActivityType=" + ActivityType.js_values() + ";"
+
+    tags = ",".join([f"{i}:'{tag['name']}'" for i, tag in EventTag.all().items()])
+    enums = enums + "const EnumEventTag={" + tags + "};"
+
     return Response(enums, mimetype="application/javascript")
