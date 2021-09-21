4f3da0455d8d9185d986567d1f7549dd0cd400eb
beaubruh <hugo.beaubrun@etu.univ-smb.fr>
2021-09-21 10:58:12 +0200

    Created equipement model try#3

--- collectives/models/equipment.py
+++ collectives/models/equipment.py
@@ -1,22 +1,19 @@
 """Module for registration related classes
 """
 from .globals import db
-
 class Equipment(db.Model):
     """Class of an equipment.
-
-    An equipment is 
+    An equipment is
     """
-
     __tablename__ = "equipments"
 
     id = db.Column(db.Integer, primary_key=True)
 
     purchaseDate = db.Column(db.DateTime, nullable=False, index=True)
-    
+
     model = db.Column(db.String(100), nullable=False)
 
-    #reference = 
+    #reference =
 
     caution = db.Column(db.Float, nullable = True)
 
