b44aa70903fc579209000583d2c2cb319c247712
hug0be <83722580+hug0be@users.noreply.github.com>
2022-01-13 00:25:34 +0100

    [FEATURE] Added leader_reservation form

---
+++ collectives/forms/reservation.py
@@ -0,0 +1,28 @@
+
+from collectives.models.role import RoleIds
+
+from ..models.user import User
+from ..models.reservation import Reservation
+from wtforms import DateField, SelectField, SubmitField
+from flask_wtf.form import FlaskForm
+
+
+class LeaderReservationForm(FlaskForm):
+    """Form for leaders to reserve equipment
+    Contrary to lambda user, they don't need to pay nor specify a return date
+    """
+
+    class Meta:
+        model = Reservation
+
+    leader_id = SelectField("Encadrant :", coerce=int, choices=[])
+
+    event_id = SelectField("Collective :", coerce=int, choices=[])
+
+    collect_date = DateField("Date d'emprunt :", format="%d/%m/%Y")
+
+    submit = SubmitField("Enregistrer")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.leader_id.choices = User.matching_roles(RoleIds.all_activity_leader_roles())
