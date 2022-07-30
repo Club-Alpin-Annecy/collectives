27fa5e55ee85fb25c550226640548675d1af0e3e
Yann P <yannc11@free.fr>
2022-07-30 21:38:04 +0200

    Correction pylint

--- collectives/__init__.py
+++ collectives/__init__.py
@@ -11,7 +11,6 @@ Typical usage example::
 
 from logging.config import fileConfig
 import werkzeug
-import datetime
 
 from flask import Flask, current_app
 from flask_assets import Environment, Bundle
