2823c756493a79283c508380cbbf0397ef18c71e
Yann P <yannc11@free.fr>
2022-07-30 21:31:43 +0200

    Génération du PDF avec xhtml2pdf

--- collectives/routes/profile.py
+++ collectives/routes/profile.py
@@ -2,10 +2,10 @@
 
 This modules contains the /profile Blueprint
 """
-from datetime import date
-from PyPDF2 import PdfFileReader, PdfFileWriter
-from PyPDF2.generic import DecodedStreamObject, EncodedStreamObject, NameObject
-from flask import send_file, abort
+from datetime import date, datetime
+from io import BytesIO
+from xhtml2pdf import pisa
+from flask import send_file
 
 from flask import flash, render_template, redirect, url_for, request
 from flask import Blueprint
@@ -134,146 +134,52 @@ def my_payments():
     return render_template("profile/my_payments.html", title="Mes paiements")
 
 
-@blueprint.route("/user/volunteer_card/<user_id>", methods=["GET"])
-def show_volunteer_card(user_id):
-    """Route to show the volunteer card of a regular user.
+@blueprint.route("/user/volunteer_card")
+def show_volunteer_card():
+    """Route to show the volunteer card of a regular user."""
+    if not current_user.has_any_role():
+        flash("Non autorisé", "error")
+        return redirect(url_for("event.index"))
 
-    :param int user_id: Primary key of the user.
-    """
-    if int(user_id) != current_user.id:
-        if not current_user.has_any_role():
-            flash("Non autorisé", "error")
-            return redirect(url_for("event.index"))
-        if not current_user.has_signed_ca():
-            flash(
-                """Vous devez signer la charte RGPD avant de pouvoir
-                     accéder à des informations des utilisateurs""",
-                "error",
-            )
-            return redirect(url_for("profile.confidentiality_agreement"))
-
-    president_role = Role.query.filter_by(role_id = "President").first()
+    president_role = Role.query.filter_by(role_id="President").first()
     if not president_role:
         # No president in roles table
-        abort(500)
+        flash(
+            """Impossible de générer l'attestation bénévole.
+                Le club n'a pas de président, merci de contacter le support.""",
+            "error",
+        )
+        return redirect(url_for("profile.show_user", user_id=current_user.id))
 
-    president = president_role.user
-    user = User.query.filter_by(id=user_id).first()
-    today = date.today()
-    if(today.month >= 9):
-        season = str(today.year) + "/" + str(today.year+1)
-    else:
-        season = str(today.year-1) + "/" + str(today.year)
+    # Render HTML template
+    html_template = render_template(
+        "attestation_benevole/attestation_benevole.html",
+        user=current_user,
+        president=president_role.user,
+        today=date.today(),
+    )
 
-    # Variables for PDF:
-    # Prénom: user.first_name
-    # Nom: user.last_name
-    # Jour: today.day
-    # Mois: today.month
-    # Année: today.year
-    # Année en cours: season
-    # Prénom Président: president.first_name
-    # Nom Président: president.last_name
+    # Generate PDF V2
+    out = BytesIO()
 
-    pdf = PdfFileReader(open("./collectives/static/caf/doc/Template_Attestion_benevole.pdf", "rb"))
-    page = pdf.getPage(0)
-    content = page.extractText()
-        
+    pisa_status = pisa.CreatePDF(html_template, dest=out)
 
-    # Arrive à décoder le PDF apparement, mais ne trouve pas le texte à remplacer
-    # generatePDF(user, today, season, president)
-    
-    return render_template("profile.html", title="Profil adhérent", user=user)
+    if pisa_status.err:
+        # Error while generating PDF file
+        flash(
+            """Impossible de générer l'attestation bénévole.""",
+            "error",
+        )
+        return redirect(url_for("profile.show_user", user_id=current_user.id))
 
-def generatePDF(user, date, season, president):
-    """Function to generate the "Attestation Bénévole" of a given user
+    out.seek(0)
 
-    :param User user: User object of the volunteer.
-    :param datetime date: Today date.
-    :param str season: Current season for the card.
-    :param User president: User object of the current president.
-    """
-
-    # Provide replacements list
-    replacements = { 'benevole': 'C\'est moi !'}
-
-    # Load PDF template
-    pdf = PdfFileReader(open("./collectives/static/caf/doc/Template_Attestion_benevole.pdf", "rb"))
-
-    # Prepare PDF output
-    writer = PdfFileWriter()
-
-    # Check on each pages of the document
-    for page_number in range(0, pdf.getNumPages()):
-        page = pdf.getPage(page_number)
-        contents = page.getContents()
-
-        if isinstance(contents, DecodedStreamObject) or isinstance(contents, EncodedStreamObject):
-            process_pdf_data(contents, replacements)
-        elif len(contents) > 0:
-            for obj in contents:
-                if isinstance(obj, DecodedStreamObject) or isinstance(obj, EncodedStreamObject):
-                    streamObj = obj.getObject()
-                    process_pdf_data(streamObj, replacements)
-
-        # Force content replacement
-        page[NameObject("/Contents")] = contents.decodedSelf
-        writer.addPage(page)
-
-    # Save the PDF (We will have to show it live, not save it)
-    with open("./collectives/static/caf/doc/test.result.pdf", 'wb') as out_file:
-        writer.write(out_file)
-
-def process_pdf_data(object, replacements):
-    """Function to decode / encode data of a pdf page
-
-    :param StreamObject object: Stream of a PDF page.
-    :param dict replacements: Dictionnary of the datas to replace.
-    """
-    data = object.getData()
-    #decoded_data = data.decode('utf-8')
-    #decoded_data = data.decode('ascii')
-    decoded_data = data.decode('iso-8859-1')
-    #decoded_data = data.decode('utf-8", "ignore')
-
-    replaced_data = replace_text(decoded_data, replacements)
-
-    #encoded_data = replaced_data.encode('utf-8')
-    encoded_data = replaced_data.encode('iso-8859-1')
-    if object.decodedSelf is not None:
-        object.decodedSelf.setData(encoded_data)
-    else:
-        object.setData(encoded_data)
-
-def replace_text(content, replacements = dict()):
-    """Function to decode / encode data of a pdf page
-
-    :param pdf_page content: Decoded pdf page.
-    :param dict replacements: Dictionnary of the datas to replace.
-    """
-    lines = content.splitlines()
-
-    result = ""
-    in_text = False
-
-    for line in lines:
-        if line == "BT":
-            in_text = True
-
-        elif line == "ET":
-            in_text = False
-
-        elif in_text:
-            cmd = line[-2:]
-            if cmd.lower() == 'tj':
-                replaced_line = line
-                for k, v in replacements.items():
-                    replaced_line = replaced_line.replace(k, v)
-                result += replaced_line + "\n"
-            else:   
-                result += line + "\n"
-            continue
-
-        result += line + "\n"
-
-    return result
+    # Show file to user
+    return send_file(
+        out,
+        mimetype="application/pdf",
+        attachment_filename=str(
+            "Attestation Benevole - " + current_user.full_name() + ".pdf"
+        ),
+        as_attachment=True,
+    )
