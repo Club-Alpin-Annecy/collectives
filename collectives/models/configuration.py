f09cc4aa68338267c9df2bfd2465a231e5fcbd59
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2023-01-31 14:51:00 +0100

    fix Blake not-callable error

--- collectives/models/configuration.py
+++ collectives/models/configuration.py
@@ -149,7 +149,7 @@ class ConfigurationItem(db.Model):
     :type: int"""
 
     date = db.Column(
-        db.DateTime, server_default=func.now()  # pylint: disable=not-callable
+        db.DateTime, server_default=func.now() # pylint: disable=not-callable
     )
     """ Edition date
 
