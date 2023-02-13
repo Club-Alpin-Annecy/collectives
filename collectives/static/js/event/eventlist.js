6e4054b85d04a06fc9be1c6bc06f48a475ebd296
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2023-02-13 19:46:17 +0100

    update waiting_list badge conditions

--- collectives/static/js/event/eventlist.js
+++ collectives/static/js/event/eventlist.js
@@ -166,9 +166,9 @@ function eventRowFormatter(row){
 }
 
 function getSlotsAvailableBadge(event) {
-    "returns 'Full' badge when no more available slots, and 'waiting list' when there are availabilities in waiting list (whatever is the number of already registered participants)"
+    "returns 'Full' badge when no more available slots, and 'waiting list' when there are availabilities in waiting list (whatever is the number of already registered"
     if (event.status != EnumEventStatus['Cancelled'])
-        if (event.has_free_waiting_slots)
+        if (event.has_free_waiting_slots && (!event.has_free_online_slots))
             return `<span class="event-status-badge event-status-waiting-list ">Liste d'attente</span>`
         else if (!event.has_free_slots && !event.has_free_waiting_slots)
             return `<span class="event-status-badge event-status-full ">Complet</span>`
