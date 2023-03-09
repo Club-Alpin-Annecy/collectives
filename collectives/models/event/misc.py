4fe55ab6686549a3b04af59583507c8293d2b07c
gdaviet <57617656+gdaviet@users.noreply.github.com>
2023-03-09 13:32:39 +0100

    [FIX] restrictions text display (#614)

--- collectives/models/event/misc.py
+++ collectives/models/event/misc.py
@@ -127,6 +127,7 @@ class EventMiscMixin:
             self._user_group = UserGroup()
         if not self._user_group.event_conditions:
             condition = GroupEventCondition(event_id=parent_event_id, is_leader=False)
+            condition.event = self.query.get(parent_event_id)
             self._user_group.event_conditions.append(condition)
         else:
             self._user_group.event_conditions[0].event_id = parent_event_id
