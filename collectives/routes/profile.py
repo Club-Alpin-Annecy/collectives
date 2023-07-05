a98a70da21cbb350671c4f234ba30588b122b457
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2023-07-05 10:55:12 +0200

    display attestation bénévole in profile

--- collectives/routes/profile.py
+++ collectives/routes/profile.py
@@ -181,7 +181,7 @@ def volunteer_certificate():
             "error",
         )
         return redirect(url_for("profile.show_user", user_id=current_user.id))
-    if not current_user.has_any_role():
+    if not current_user.has_any_role() and not current_user.is_benevole():
         flash("Non autorisé", "error")
         return redirect(url_for("event.index"))
 
